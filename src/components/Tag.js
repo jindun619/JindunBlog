a3fab86de7611050ce5d9b4a9532a409ce590e35
jindun619 <jindun619@naver.com>
2023-09-16 05:54:50 +0000

    2

--- src/components/Tag.js
+++ src/components/Tag.js
@@ -1,9 +1,15 @@
 import * as React from "react";
 
 function Tag({ name, size }) {
-  return (
-    <button className={`btn btn-${size} btn-outline`}>{name}</button>
-  );
+  if(size ==="xs") {
+    return (
+      <button className="btn btn-xs btn-outline">{name}</button>
+    )
+  } else if(size === "sm") {
+    return (
+      <button className="btn btn-sm btn-outline">{name}</button>
+    )
+  }
 }
 
 export { Tag };
