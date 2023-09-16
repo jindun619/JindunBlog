9a2f9927fad4e6e2c30d8c1b7ad74410145aed08
jindun619 <jindun619@naver.com>
2023-09-16 06:00:14 +0000

    btn normal-case

--- src/components/Tag.js
+++ src/components/Tag.js
@@ -3,11 +3,11 @@ import * as React from "react";
 function Tag({ name, size }) {
   if(size ==="xs") {
     return (
-      <button className="btn btn-xs btn-outline">{name}</button>
+      <button className="btn btn-xs btn-outline normal-case">{name}</button>
     )
   } else if(size === "sm") {
     return (
-      <button className="btn btn-sm btn-outline">{name}</button>
+      <button className="btn btn-sm btn-outline normal-case">{name}</button>
     )
   }
 }
