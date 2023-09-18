82144a0bff34709345e2152146bdda6257f7df1d
jindun619 <jindun619@naver.com>
2023-09-18 10:37:55 +0000

    DEV_SSR enabled

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,9 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
+  flags: {
+    DEV_SSR: true
+  },
   siteMetadata: {
     title: `Jindun Blog`,
     description: `This is a blog`,
