47b10f45a042b48abc79a0a0cd0cb2a84dd2449c
jindun619 <jindun619@naver.com>
2023-09-26 11:48:01 +0000

    2

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -5,7 +5,7 @@ function Bio() {
         <div className="flex ml-4">
           <div className="avatar">
             <div className="w-20 h-20 mt-4s mr-4 rounded-full border-2 border-primary">
-              <img src="https://pann.nate.com/attach/imageView?source_url=http%3A%2F%2Ffimg5.pann.com%2Fnew%2Fdownload.jsp%3FFileID%3D54519978&cate_cd=30101" alt="bioImg" />
+              <img src="https://mblogthumb-phinf.pstatic.net/MjAyMTA4MTFfMzkg/MDAxNjI4NjY1NjgwNTUw.K2a44KxCgskoaKSw8cH5ySnsEuadVA8wphcrBOrDwBQg.R4GfkzCRdTa1jdicp9p4Ph8A4THJ8tX1mZO-uTqzgygg.JPEG.bbekimha/%EB%A3%A8%ED%94%BC.jpg?type=w800" alt="bioImg" />
             </div>
           </div>
           <div>
@@ -22,5 +22,3 @@ function Bio() {
 }
 
 export { Bio }
-
-//ttps://yapcarforum.kr/bbs/view_image.php?bo_table=free&fn=2022203540_pZAV1Lru_c558836e70450ceee3862890da927523f670ddc5.jpg
