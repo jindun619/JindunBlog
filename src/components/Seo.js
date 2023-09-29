0d453d986a228ed239c631a60b3d5377b0ad5434
jindun619 <jindun619@naver.com>
2023-09-29 00:54:47 +0000

    favicon added

--- src/components/Seo.js
+++ src/components/Seo.js
@@ -22,15 +22,18 @@ function Seo ({ title, description, url }) {
 		<Helmet>
 			<title>{`${title} | ${siteMetadata.title}`}</title>
 			<meta name="description" content={description} />
+      <meta property="og:title" content={title} />
+      <meta property="og:url" content={`${siteMetadata.siteUrl}${url}`} />
+      {/* <meta property="og:image" content={image} /> */}
+      <meta property="og:description" content={description} />
 
-            <meta property="og:title" content={title} />
-            <meta property="og:url" content={`${siteMetadata.siteUrl}${url}`} />
-            {/* <meta property="og:image" content={image} /> */}
-            <meta property="og:description" content={description} />
+      {/* Search Engine Verifications */}
+      <meta name="google-site-verification" content="_ugXUv4-9ZFkQIhcRLxyyHKcnw1eQKy6qIrko9xhsak" />
+      <meta name="naver-site-verification" content="1ac14aae744cb6ea0e41eb304d628d302ba07e60" />
 
-            {/* Search Engine Verifications */}
-            <meta name="google-site-verification" content="_ugXUv4-9ZFkQIhcRLxyyHKcnw1eQKy6qIrko9xhsak" />
-            <meta name="naver-site-verification" content="1ac14aae744cb6ea0e41eb304d628d302ba07e60" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/asset/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/asset/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/asset/favicon-16x16.png" />
 		</Helmet>
 	);
 };
